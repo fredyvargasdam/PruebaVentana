442e039c3a4568aa56d8ddd522c0235724a1d8ce
Fredy <Fredy@DESKTOP-EDJL659>
2021-01-08 01:56:28 +0100

    Casi pero NO

--- src/pruebaventana/ReservasVendedorController.java
+++ src/pruebaventana/ReservasVendedorController.java
@@ -5,20 +5,32 @@
  */
 package pruebaventana;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
+import javax.imageio.ImageIO;
+import modelo.BirthdayEvent;
+import modelo.DatePickerCell;
+import modelo.EstadoReserva;
+import modelo.Producto;
+import modelo.Reserva;
 
 /**
  * FXML Controller class
@@ -36,32 +48,91 @@ public class ReservasVendedorController implements Initializable {
     @FXML
     private AnchorPane apReservasVendedor;
     @FXML
-    private TableView<?> tvReservas;
+    private TableView<Reserva> tvReservas;
     @FXML
-    private TableColumn<?, ?> tcIdReserva;
+    private TableColumn<Reserva, Long> tcIdReserva;
     @FXML
-    private TableColumn<?, ?> tcUsuario;
+    private TableColumn<Reserva, String> tcUsuario;
     @FXML
-    private TableColumn<?, ?> tcProducto;
+    private TableColumn<Reserva, String> tcProducto;
     @FXML
-    private TableColumn<?, ?> tcCantidad;
+    private TableColumn<Reserva, Integer> tcCantidad;
     @FXML
-    private TableColumn<?, ?> tcDescripcion;
+    private TableColumn<Reserva, String> tcDescripcion;
     @FXML
-    private TableColumn<?, ?> tcFecha;
+    private TableColumn<Reserva, Timestamp> tcFecha;
     @FXML
-    private TableColumn<?, ?> tcRealizada;
+    private TableColumn<Reserva, Timestamp> tcRealizada;
     @FXML
-    private TableColumn<?, ?> tcEntrega;
+    private TableColumn<Reserva, Timestamp> tcEntrega;
+   //  private TableColumn<BirthdayEvent, Timestamp> tcEntrega;
     @FXML
-    private TableColumn<?, ?> tcEstado;
+    private TableColumn<Reserva, EstadoReserva> tcEstado;
+    private List<Reserva> reservas = new ArrayList<>();
+
+    private List<Reserva> getReservas() throws IOException {
+        List<Reserva> reservas = new ArrayList<>();
+        Reserva reserva;
+        Producto producto;
+        for (int i = 0; i < 20; i++) {
+            producto = new Producto();
+            producto.setPrecio(179.99f);
+            producto.setModelo("Nike Air Max 97");
+            producto.setImagen(extractBytes("E:\\reto2\\PruebaVentana\\src\\pruebaventana\\"));
+
+            reserva = new Reserva();
+            reserva.setId(Long.valueOf(i));
+            reserva.setCantidad(i);
+            reserva.setUser("Pimp flakito");
+            reserva.setDescripcion("Buena reserva BRO");
+            reserva.setEstado(EstadoReserva.EXPIRADA);
+            reserva.setProducto(producto);
+            reserva.setFechaReserva(new Timestamp(System.currentTimeMillis()));
+            reserva.setFechaEntrega(new Timestamp(System.currentTimeMillis()));
+
+            reservas.add(reserva);
+        }
+        return reservas;
+    }
+
+    public byte[] extractBytes(String path) throws IOException {
+        // abrimos la imagen
+        File imgPath = new File(path);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream(1000);
+        BufferedImage img = ImageIO.read(new File(path, "nik.jpg"));
+        ImageIO.write(img, "jpg", baos);
+        baos.flush();
+        //   String base64String = Base64.encode(baos.toByteArray());
+        baos.close();
+
+        return (baos.toByteArray());
+    }
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+        try {
+            reservas.addAll(getReservas());
+
+            tcIdReserva.setCellValueFactory(new PropertyValueFactory<Reserva, Long>("id"));
+            tcUsuario.setCellValueFactory(new PropertyValueFactory<Reserva, String>("user"));
+            tcProducto.setCellValueFactory(new PropertyValueFactory<Reserva, String>("producto"));
+            tcCantidad.setCellValueFactory(new PropertyValueFactory<Reserva, Integer>("cantidad"));
+            tcDescripcion.setCellValueFactory(new PropertyValueFactory<Reserva, String>("descripcion"));
+            tcRealizada.setCellValueFactory(new PropertyValueFactory<Reserva, Timestamp>("fechaReserva"));
+            tcEntrega.setCellValueFactory(new PropertyValueFactory<Reserva, Timestamp>("fechaEntrega"));
+          
+           tcEstado.setCellValueFactory(new PropertyValueFactory<Reserva, EstadoReserva>("estado"));
+         //     tcEstado.setCellValueFactory(new PropertyValueFactory<ChoiceBox<EstadoReserva>, EstadoReserva>("estado"));  
+    reservas.forEach((r) -> {
+    tvReservas.getItems().add(r);
+            });
+        } catch (IOException ex) {
+            Logger.getLogger(ReservasVendedorController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
 }
